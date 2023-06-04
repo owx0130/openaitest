e61dd44bf623521028acb80969a6203d0c53a7c9
Ong Wei Xiang <110607839+owx0130@users.noreply.github.com>
2023-06-04 12:33:55 +0800

    Delete openai_frontend directory

--- openai_frontend/src/components/sidebar.jsx
+++
@@ -1,8 +0,0 @@
-export default function Sidebar() {
-  return (
-    <div className="sidebar">
-      <button className="newChatButton">+ New Chat</button>
-      <p>ChatGPT Clone using OpenAI API</p>
-    </div>
-  )
-}
