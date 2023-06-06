9340c4048d6adb1b8d3a96dbc7aaea271983f5a8
owx0130 <weixiang0130@gmai.com>
2023-06-06 18:04:33 +0800

    new additions

--- backend/server.js
+++ backend/server.js
@@ -2,53 +2,79 @@ const PORT = 8000
 const express = require("express")
 const cors = require("cors")
 const request = require("request")
+const { htmlToText } = require("html-to-text")
 const { Configuration, OpenAIApi } = require("openai")
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-const articleUrl = "https://www.inoreader.com/stream/user/1005506540/tag/Cyber-Info/view/json"
 const API_KEY = "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"
-const arr = []
+const prevChats = []
+var newarticle = ""
 
-const configuration = new Configuration({
-  apiKey: API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const configuration = new Configuration({ apiKey: API_KEY })
+const openai = new OpenAIApi(configuration)
 
 app.post("/completions", async (req, res) => {
 
   try {
-    if (req.body.content === "") {
-      throw new Error("No prompt provided!");
+    let prompt = JSON.stringify(req.body.content)
+    let start = prompt.indexOf("http")
+    console.log(start)
+    //Isolate and remove empty prompts
+    if (prompt === "") {
+      throw new Error("No prompt provided!")
+    } else if (start >= 0) {
+      console.log("2")
+
+      //If prompt contains a URL, replace it with text data to be read by the API
+      let articleUrl = prompt.substring(start, prompt.slice(start).indexOf(" ") + start)
+      console.log(articleUrl, "1")
+      request({
+        url: articleUrl,
+        method: "GET",
+      }, (error, response, body) => {
+        console.log("4")
+        const article = JSON.parse(body)
+        console.log("5")
+        newarticle = htmlToText(JSON.stringify(article.items[0].content_html).replace(/<[^>]+>/g,""))
+        console.log(newarticle)
+      })
+      
+      let newcontent = req.body.content.replace(articleUrl, newarticle)
+      req.body.content = newcontent
+      console.log(req.body, "6")
     }
-    arr.push(req.body)
+
+    prevChats.push(req.body)
+    console.log(prevChats)
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: arr,
+      messages: prevChats
+    ,
       max_tokens: 10,
       temperature: 0,
     });
-    arr.push(response.data.choices[0].message);
-    console.log(arr);
+    console.log("hi")
+    prevChats.push(response.data.choices[0].message);
+    console.log(prevChats);
     res.send(response.data);
   } catch (error) {
     console.error(error.message);
   }
-});
-
-app.get("/", (req, res) => {
-  res.end(JSON.stringify(arr))
 })
+
+app.get("/", (req, res) => {res.end(JSON.stringify(prevChats))})
 
 app.get("/getarticle", (req, res) => {
   request({
     url: articleUrl,
     method: "GET",
   }, (error, response, body) => {
-    let article = JSON.parse(body)
+    const article = JSON.parse(body)
+    const newarticle = htmlToText(JSON.stringify(article.items[0].content_html).replace(/<[^>]+>/g,""))
     res.end(body)
-    console.log(article.items[0])
+    console.log(newarticle)
   })
 })
 
--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -2,6 +2,8 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <button className="newChatButton">+ New Chat</button>
+      <p style={{color:"whitesmoke", maxWidth: "75%"}}>If you want to use data from an online article, pass in the 
+      full URL of the article as such: https://.....</p>
       <p style={{color:"whitesmoke"}}>ChatGPT Clone using OpenAI API</p>
     </div>
   )
